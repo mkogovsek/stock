37e06616c64dbaf848edbb1ae019c2e9c24efaf1
mkogovsek <maxime.kogovsek@gmail.com>
2020-10-20 14:11:12 -0400

    Trying to calculate balanced accuracy using Y as Yesterday, and X as Yesterday-30

--- src/IO/get_data_from_yahoo.py
+++ src/IO/get_data_from_yahoo.py
@@ -5,7 +5,7 @@ from yahoo_fin import stock_info as si
 
 def get_last_stock_price(ticker, last=False):
     if last:
-        now = datetime.now()
+        now = datetime.now() - timedelta(days=1)
         start_date = now - timedelta(days=30)
         return si.get_data(ticker, start_date)
     return si.get_data(ticker)
